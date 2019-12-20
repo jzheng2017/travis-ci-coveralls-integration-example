ed89850a69e183ca48cc34b2ac7344cc86bedf0e
Jiankai Zheng <jk.zheng@hotmail.com>
2019-12-20 15:51:38 +0100

    add: index controller

--- src/main/java/com/tccie/test/controllers/IndexController.java
+++ src/main/java/com/tccie/test/controllers/IndexController.java
@@ -1,5 +1,16 @@
 package com.tccie.test.controllers;
 
-@
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class IndexController {
+
+    @GetMapping("hello-world")
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("Hello World");
+    }
 }
